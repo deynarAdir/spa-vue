50081e6b847b258646f3046577d9e8055ba7db8c
deynarAdir <deynaradirmt10@gmail.com>
2020-05-03 18:37:23 -0400

    second

--- public/js/app.js
+++ public/js/app.js
@@ -40011,7 +40011,7 @@ var render = function() {
             ? _c("div", { staticClass: "form-group" }, [
                 _c("p", { staticClass: "text-danger" }, [
                   _vm._v(
-                    "\n\t\t\t\t\t\t" + _vm._s(_vm.authErrors) + "\n\t\t\t\t\t"
+                    " \n\t\t\t\t\t\t" + _vm._s(_vm.authErrors) + "\n\t\t\t\t\t"
                   )
                 ])
               ])
@@ -91150,15 +91150,14 @@ __webpack_require__.r(__webpack_exports__);
 /*!************************************************!*\
   !*** ./resources/js/components/excel/Main.vue ***!
   \************************************************/
-/*! no static exports found */
+/*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Main_vue_vue_type_template_id_97cc1b88___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Main.vue?vue&type=template&id=97cc1b88& */ "./resources/js/components/excel/Main.vue?vue&type=template&id=97cc1b88&");
 /* harmony import */ var _Main_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Main.vue?vue&type=script&lang=js& */ "./resources/js/components/excel/Main.vue?vue&type=script&lang=js&");
-/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _Main_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__) if(__WEBPACK_IMPORT_KEY__ !== 'default') (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _Main_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[key]; }) }(__WEBPACK_IMPORT_KEY__));
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
 
 
@@ -91188,7 +91187,7 @@ component.options.__file = "resources/js/components/excel/Main.vue"
 /*!*************************************************************************!*\
   !*** ./resources/js/components/excel/Main.vue?vue&type=script&lang=js& ***!
   \*************************************************************************/
-/*! no static exports found */
+/*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
